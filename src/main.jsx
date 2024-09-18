9728879c0e57268275d32f099f90986587b9ae72
taro <taro_n1@hotmail.com>
2024-09-18 10:16:12 +0700

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+import 'font-awesome/css/font-awesome.min.css';
+
+// // Bootstrap CSS
+// import "bootstrap/dist/css/bootstrap.min.css";
+// // Bootstrap Bundle JS
+// import "bootstrap/dist/js/bootstrap.bundle.min";
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
